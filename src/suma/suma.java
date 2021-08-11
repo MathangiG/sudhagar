2843c0c1f6fcd3dabc3efb0a04674f64b0c0d62a
MathangiG <gmathangibtech@gmail.com>
2021-08-11 15:53:39 +0530

    https://github.com/MathangiG/sudhagar.git

--- src/suma/suma.java
+++ src/suma/suma.java
@@ -3,13 +3,20 @@ package suma;
 public class suma {
 	int ab;
 	
-	public void m1() {
+	public void m77() {
 	
 		System.out.println("Method 1 in class b");
 	}
 	
 
-	public void m2() {
+	public void m66() {
+		
+		System.out.println("Method 2 in class b");
+	}
+	public void m689() {
+		
+		System.out.println("Method 2 in class b");
+	}	public void m68() {
 		
 		System.out.println("Method 2 in class b");
 	}
