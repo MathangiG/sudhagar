d63a80a0f30db8fbfbfc77b5a39e25839fad52ce
MathangiG <gmathangibtech@gmail.com>
2021-08-11 15:22:33 +0530

    my name

---
+++ src/suma/suma.java
@@ -0,0 +1,16 @@
+package suma;
+
+public class suma {
+	int ab;
+	
+	public void m1() {
+	
+		System.out.println("Method 1 in class b");
+	}
+	
+
+	public void m2() {
+		
+		System.out.println("Method 2 in class b");
+	}
+}
